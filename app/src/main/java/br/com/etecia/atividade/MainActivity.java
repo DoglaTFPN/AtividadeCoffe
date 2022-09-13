ffe74d634fb297072db85a8a02450a799ca8f505
DoglaTFPN <douglas_saragoza@hotmail.com>
2022-09-13 20:21:58 -0300

    Commit 13/09/22

---
+++ app/src/main/java/br/com/etecia/atividade/MainActivity.java
@@ -0,0 +1,54 @@
+package br.com.etecia.atividade;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    fragmentCoffe coffefragment = new fragmentCoffe();
+    fragmentTea fragmentTea = new fragmentTea();
+    fragmentMuffin fragmentMuffin = new fragmentMuffin();
+    fragmentDonuts fragmentDonuts = new fragmentDonuts();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, coffefragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mCoffe:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, coffefragment).commit();
+                        return true;
+                    case R.id.mDonuts:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentDonuts).commit();
+                        return true;
+                    case R.id.mMuffin:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentMuffin).commit();
+                        return true;
+                    case R.id.mTea:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentTea).commit();
+                        return true;
+                }
+
+                return false;
+            }
+
+        });
+
+
+    }
+}
