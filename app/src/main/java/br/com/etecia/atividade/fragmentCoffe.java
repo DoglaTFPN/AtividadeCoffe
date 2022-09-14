b9bb1d0e5effb9be7664c8f8b1778b694b933b60
DoglaTFPN <douglas_saragoza@hotmail.com>
2022-09-13 22:19:18 -0300

    Commit 13/09/22 part 3

--- app/src/main/java/br/com/etecia/atividade/fragmentCoffe.java
+++ app/src/main/java/br/com/etecia/atividade/fragmentCoffe.java
@@ -3,6 +3,7 @@ package br.com.etecia.atividade;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,10 +26,11 @@ public class fragmentCoffe extends Fragment {
         lstCafes = new ArrayList<>();
 
         lstCafes.add(new Cafe("Black Cock",R.drawable.coffeimg, "café gostoso pra carai"));
-        lstCafes.add(new Cafe("Pingado",R.drawable.coffeimg, "café gostoso pra carai"));
-        lstCafes.add(new Cafe("Black Cock",R.drawable.coffeimg, "café gostoso pra carai"));
-        lstCafes.add(new Cafe("Black Cock",R.drawable.coffeimg, "café gostoso pra carai"));
+        lstCafes.add(new Cafe("Pingado",R.drawable.cafepingado, "café gostoso pra carai"));
+        lstCafes.add(new Cafe("Black cofi",R.drawable.coffeimg, "café gostoso pra carai"));
+        lstCafes.add(new Cafe("CAFEZIN",R.drawable.cafepingado, "café gostoso pra carai"));
 
+        RecyclerView mRecyclerView = findViewById(R.id.recyclerCoffe);
 
         return v;
     }
